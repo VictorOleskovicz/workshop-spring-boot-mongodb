a7944bf3ce85577520d45a7ad37084862a15aaa8
victoroleskovicz <victoroleskovicz1@gmail.com>
2024-02-22 21:48:36 -0300

    Primeiro commit

---
+++ src/test/java/com/victorolesk/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victorolesk.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
