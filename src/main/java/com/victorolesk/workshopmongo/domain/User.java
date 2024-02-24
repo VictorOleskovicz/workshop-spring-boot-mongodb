0e3c30608bc617d7b645513c2816bb23d1998669
victoroleskovicz <victoroleskovicz1@gmail.com>
2024-02-23 21:40:07 -0300

    conectando ao mongodb com repostory e service

--- src/main/java/com/victorolesk/workshopmongo/domain/User.java
+++ src/main/java/com/victorolesk/workshopmongo/domain/User.java
@@ -3,10 +3,15 @@ package com.victorolesk.workshopmongo.domain;
 import java.io.Serializable;
 import java.util.Objects;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
-	private Integer id;
+	@Id
+	private String id;
 	private String name;
 	private String email;
 	
@@ -14,14 +19,14 @@ public class User implements Serializable {
 		
 	}
 
-	public User(Integer id, String name, String email) {
+	public User(String id, String name, String email) {
 		super();
 		this.id = id;
 		this.name = name;
 		this.email = email;
 	}
 
-	public Integer getId() {
+	public String getId() {
 		return id;
 	}
 
@@ -33,7 +38,7 @@ public class User implements Serializable {
 		return email;
 	}
 
-	public void setId(Integer id) {
+	public void setId(String id) {
 		this.id = id;
 	}
 
---
+++ src/main/java/com/victorolesk/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.victorolesk.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.victorolesk.workshopmongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+}
