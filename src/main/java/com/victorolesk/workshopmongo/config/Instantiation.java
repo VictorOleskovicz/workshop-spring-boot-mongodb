8aeb0f792c5bd5b325b20f47db9dd212c9154f2e
victoroleskovicz <victoroleskovicz1@gmail.com>
2024-02-29 20:47:32 -0300

    Post com user aninhado

--- src/main/java/com/victorolesk/workshopmongo/config/Instantiation.java
+++ src/main/java/com/victorolesk/workshopmongo/config/Instantiation.java
@@ -1,31 +1,44 @@
 package com.victorolesk.workshopmongo.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.victorolesk.workshopmongo.domain.Posts;
 import com.victorolesk.workshopmongo.domain.User;
+import com.victorolesk.workshopmongo.repository.PostRepository;
 import com.victorolesk.workshopmongo.repository.UserRepository;
 
 @Configuration
 public class Instantiation implements CommandLineRunner {
+	
+	SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Autowired
+	private PostRepository postRepository;
 	
 	@Override
 	public void run(String... args) throws Exception {
 		
 		userRepository.deleteAll();
+		postRepository.deleteAll();
 		
 		User maria = new User(null, "Maria Brown", "maria@gmail.com"); 
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		userRepository.saveAll(Arrays.asList(maria, alex, bob))
-		;
+		Posts post1 = new Posts(null, sdf.parse("28/10/2017"), "Travelling", "Just reached Madrid!", maria);
+		Posts post2 = new Posts(null, sdf.parse("20/10/2018"), "Cooking", "Made some cookies!", maria);
+		
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		postRepository.saveAll(Arrays.asList(post1, post2));
+		
 	}
 
 }
---
+++ src/main/java/com/victorolesk/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.victorolesk.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.victorolesk.workshopmongo.domain.Posts;
+
+@Repository
+public interface PostRepository extends MongoRepository<Posts, String> {
+
+}
